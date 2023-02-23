9d5f34b085ae0f69d9e5b383b4880395e30087b2
onasheep <inabass@naver.com>
2023-02-23 23:06:23 +0900

    Feat : make Test branch and simple item select UI

--- Assets/Undead Survivor/Scripts/Manager/GameManager.cs
+++ Assets/Undead Survivor/Scripts/Manager/GameManager.cs
@@ -6,16 +6,16 @@ public class GameManager : MonoBehaviour
 {
     
     public static GameManager instance;
-    [Header("# Gmae Control")]
+    [Header("# Game Control")]
     public float gameTime;
-    public float maxGameTime = 2 * 10f;
+    public float maxGameTime = 600f;
     [Header("# Player Info")]
     public int health;
     public int maxHealth = 100;
     public int level;
     public int kill;
     public int exp;
-    public int[] nextExp = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    public int[] nextExp = new int[15];
     [Header("# Game Object")]
     public PoolManager pool;
     public Player player;
@@ -23,11 +23,17 @@ public class GameManager : MonoBehaviour
     void Awake()
     {
         instance = this;
+        for (int i = 0; i < nextExp.Length; i++)
+        {
+            CalculateLevel(i);
+        }
     }
 
     void Start()
     {
         health = maxHealth;
+
+        
     }
     void Update()
     {
@@ -50,5 +56,12 @@ public class GameManager : MonoBehaviour
             exp = 0;
 
         }
+    }
+
+    public void CalculateLevel(int index)
+    {
+        const int setLevel = 10;
+        int levelWeight = 5;
+        nextExp[index] = Mathf.FloorToInt(setLevel+ Mathf.Pow(levelWeight * index, 1.5f));      
     }
 }
